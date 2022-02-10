55c15793e73c36c7696976602e01a92c139392f8
Kazuki Shin <kazukis2@illinois.edu>
2022-02-10 12:51:07 -0600

    add generate massive dataset, train with generated data

--- scripts/nvisii_data_gen/single_video_pybullet.py
+++ scripts/nvisii_data_gen/single_video_pybullet.py
@@ -318,7 +318,7 @@ for i_obj in range(int(opt.nb_objects)):
 
     toy_to_load = google_content_folder[random.randint(0,len(google_content_folder)-1)]
 
-    obj_to_load = toy_to_load + "/model.stl"
+    obj_to_load = toy_to_load + "/meshes/model.stl"
     texture_to_load = toy_to_load + "/texture_map.png"
     name = "kimlab_" + toy_to_load.split('/')[-2] + f"_{i_obj}"
     adding_mesh_object(name,obj_to_load,texture_to_load,scale=0.001)
@@ -507,10 +507,10 @@ while condition:
             file_path = f"{opt.outf}/{str(i_render).zfill(5)}.depth.exr"
         )
 
-# subprocess.call(['ffmpeg', '-y',\
-#     '-framerate', '30', "-hide_banner", "-loglevel", \
-#     "panic",'-pattern_type', 'glob', '-i',\
-#     f"{opt.outf}/*.png", f"{opt.outf}/video.mp4"]) 
+subprocess.call(['ffmpeg', '-y',\
+    '-framerate', '30', "-hide_banner", "-loglevel", \
+    "panic",'-pattern_type', 'glob', '-i',\
+    f"{opt.outf}/*.png", f"{opt.outf}/video.mp4"]) 
 
 visii.deinitialize()
 
