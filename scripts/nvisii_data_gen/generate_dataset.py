bd7b5cb2ba242d306fb14bf5f18227e55307676d
Kazuki Shin <kazukis2@illinois.edu>
2022-04-24 15:59:20 -0500

    update params for SDG and train main scripts

--- scripts/nvisii_data_gen/generate_dataset.py
+++ scripts/nvisii_data_gen/generate_dataset.py
@@ -7,24 +7,17 @@ import ipdb
 
 for i in range(0,100):
 	to_call = [
-		# "python",'single_video.py',
 		"python3",'single_video_pybullet.py',
 		'--path_single_obj', 'models/cup/meshes/model.stl',
 		'--path_single_obj_texture', 'models/cup/texture_map.png',
-		'--spp','100',
+		'--spp','400',
 		'--nb_frames', '200',
-		'--nb_objects', str(int(random.uniform(10,15))),
+		'--nb_objects', str(int(random.uniform(2,10))),
 		'--nb_distractors', str(int(random.uniform(0,3))),
-		# '--static_camera',
-		# '--nb_frames', '1',
-		# '--nb_objects',str(1),
-		'--outf',f"dataset_cup_20k/{str(i).zfill(3)}",
+		'--height', '480',
+		'--width', '640',
+		'--outf',f"dataset_cup_kimlab_20k/{str(i).zfill(3)}",
+		'--skyboxes_folder','dome_hdri_haven/selected_hdri/',
+		# '--interactive'
 	]
 	subprocess.call(to_call)
-	# subprocess.call(['mv',f'dataset/{str(i).zfill(3)}/video.mp4',f"dataset/{str(i).zfill(3)}.mp4"])
-	# break
-	# subprocess.Popen(["rsync",'-r',f'output/dataset/{str(i).zfill(3)}',
-	# 	"/mnt/adlr/dataset_2/",";",
-	# 	'rm','-rf',f'output/dataset/{str(i).zfill(3)}'])
-	# subprocess.call([])
-	# break
--- scripts/train2/run_training.py
+++ scripts/train2/run_training.py
@@ -4,12 +4,13 @@ to_call = [
 	"python3", '-m', 'torch.distributed.launch',
 	'--nproc_per_node', '1', 'train.py',
 	'--network', 'dope',
-	'--epochs', '200',
+	'--epochs', '100',
 	'--batchsize', '128',
-	'--subbatchsize', '10',
-	'--gpuids', '0',
+	'--subbatchsize', '100',
+	'--gpuids', '0', '1',
 	'--objects', 'obj',
-	'--outf', 'cup',
-	'--data', '../nvisii_data_gen/output/dataset_cup_20k',
+	'--outf', 'bowl',
+	'--data', '../nvisii_data_gen/output/dataset_bowl_20k',
+	'--net', '/home/kimlab/catkin_ws/src/Deep_Object_Pose/weights/bowl_9e.pth',
 ]
 subprocess.call(to_call)
